c397da973a7fb421aeb29843c47ff5e11038e816
josejonatasoliveira <32471229+josejonatasoliveira@users.noreply.github.com>
2018-05-08 09:16:38 -0300

    Update OpenCageApi.py

--- OpenCageApi.py
+++ OpenCageApi.py
@@ -1,17 +1,19 @@
+# Codigo Python para pegar a latitude e longitude de endereços de tabelas do SqlServer atraves da API OpenCage
+#
 from geopy.geocoders import OpenCage
-opencage = OpenCage('e5dd1925dee24d7ebb0bdb5ada759600', domain='api.opencagedata.com', scheme='https', timeout=None, proxies=None, user_agent=None)
+opencage = OpenCage('suaApiGeradaNoSiteOpenCage.com', domain='api.opencagedata.com', scheme='https', timeout=None, proxies=None, user_agent=None)
 import pypyodbc as pyodbc
 
 #Faz a conexão com o banco do Sql Server
-connection = 'Driver={SQL Server};Server=10.50.1.95;Database=jose;UID=soumais;PWD=sou2017mais'
+connection = 'Driver={SQL Server};Server=10.x.x.x;Database=nomeDatabase;UID=user;PWD=123'
 db = pyodbc.connect(connection)
 
 
 cursor = db.cursor()
-cursor.execute('select top 2500 l.endereco,l.geolocalizacao from localizacoes l')
+cursor.execute('select endereco from cadastro')
 
 for row in cursor.fetchall():
-    localizacao = list(opencage.geocode(list(row)[0],exactly_one=True))[1]
+    localizacao = list(opencage.geocode(row,exactly_one=True))[1]
     print(localizacao)
 
 
